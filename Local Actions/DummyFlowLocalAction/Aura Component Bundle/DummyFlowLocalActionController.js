e0f0d3b0bce3b9c11f7220bf458706a4e02a8185
Nads <sfdcnads@gmail.com>
2020-02-18 03:45:14 +0100

    Removed DummyFlowLocalAction

--- Local Actions/DummyFlowLocalAction/Aura Component Bundle/DummyFlowLocalActionController.js
+++
@@ -1,7 +0,0 @@
-({
-    /* When a flow executes this component, it calls the invoke method */
-    invoke : function(component, event, helper) {
-       /* This is just a dummy action, so we can just keep it blank. */
-       //console.log('Dummy flow action called.');
-    }
- })
