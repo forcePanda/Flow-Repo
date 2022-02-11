52fd2c7d2afc5acba51eb6c36af7c3a8ae7a49c3
ForcePanda <forcepanda07@gmail.com>
2022-02-11 21:15:30 +0100

    Added dependent picklist w/ record type

---
+++ Screen Components/Dependent Picklist with Record Type/README.md
@@ -0,0 +1,26 @@
+# Flow Screen Component: Dependent Picklist with Record Type
+A flow screen component to setup dependent picklist fields with record type.
+
+Related blog post: <TODO>
+
+
+### Input/Output(s) for component
+|Parameter	               |Use for Input	   |Use for Output	   |Description 
+|-|-|-|-|
+| Controlling Field | ✔ |  | API Name of the controlling field |
+| Dependent Field | ✔ |  | API name of the dependent field |
+| Object Name | ✔ |  | API name of the object |
+| Default RecordType Id | ✔ |  | Id of the default record type |
+| Hide RecordType Input | ✔ |  | Hide/Show the record type input field |
+| Disable RecordType Selection | ✔ |  | Disable the record type selection field |
+| Controlling Field Value |  | ✔ | Selected Controlling Field Value |
+| Dependent Field Value |  | ✔ | Selected Dependent Field Value |
+| Selected RecordType Id |  | ✔ | Selected RecordType Id |
+
+### Unlocked package link(s): 
+
+| Version | Fixes |Package Link*	    
+|-|-|-|
+| 1.0 | Base version | https://login.salesforce.com/packaging/installPackage.apexp?p0=04t6F000004PCSuQAO |
+
+*To install the package in sandbox, replace login.salesforce.com with test.salesforce.com.
---
+++ Screen Components/Dependent Picklist with Record Type/lwc/dependentPicklistWithRecordType/dependentPicklistWithRecordType.js
@@ -0,0 +1,89 @@
+import { LightningElement, wire, api, track } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+
+export default class DependentPicklistWithRecordType extends LightningElement {
+
+    @api controllingField;
+    @api dependentField;
+    @api userDefaultRecordTypeId;
+    @api objectApiName='Account';
+    @api disableRecordTypeSelection;
+    @api hideRecordTypeInput;
+    @api controllingFieldValue;
+    @api dependentFieldValue;
+    @api recordTypeId;
+
+    objectInfo;
+
+    @wire(getObjectInfo, { objectApiName: '$objectApiName' })
+    handleObjectInfoWire({error, data}) {
+
+        if(error) {
+            this.handleError(error);
+            return;
+        }
+
+        if(!data) {
+            return;
+        }
+
+        this.objectInfo = data;
+        this.initRecordTypeId();
+    }
+
+    initRecordTypeId() {
+
+        if(!this.objectInfo) {
+            return;
+        }
+
+        this.recordTypeId = this.userDefaultRecordTypeId 
+            ? Object.keys(this.objectInfo.recordTypeInfos).find(rtId => rtId === this.userDefaultRecordTypeId )
+            : this.defaultRecordTypeId;
+    }
+
+    handleRTChange(event) {
+        this.recordTypeId = event.detail.value;
+    }
+    
+    handleControllingPicklistChange(event) {
+        this.controllingFieldValue = event.detail.value;
+    }
+
+    handleDependentPicklistChange(event) {
+        this.dependentFieldValue = event.detail.value;
+    }
+
+    handleError(err) {
+        console.error(err);
+    }
+
+    get recordTypesList() {
+        if(!this.objectInfo) {
+            return;
+        }
+
+        const rtInfoList = this.objectInfo.recordTypeInfos;
+        let rtList = [];
+        Object.keys(this.objectInfo.recordTypeInfos).forEach(rt => {
+            let rtInfo = rtInfoList[rt];
+            rtInfo.master || !rtInfo.available || rtList.push(this.getRecordTypeOption(rtInfo));
+        });
+        return rtList;
+    }
+
+    getRecordTypeOption(rtInfo) {
+        return { 
+            label : rtInfo.name, 
+            value: rtInfo.recordTypeId 
+        };
+    }
+
+    get defaultRecordTypeId() {
+        return this.objectInfo?.defaultRecordTypeId;
+    }
+
+    get showRecordTypeInput() {
+        return this.recordTypeId && !this.hideRecordTypeInput;
+    }
+}
